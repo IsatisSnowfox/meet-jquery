498e2f9666f731d98c94ac640aa9b8b2d6dbf949
IsatisSnowfox <isatis.snowfox@gmail.com>
2017-02-13 13:50:10 +0100

    Replace Grunt with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+/*
+
+Isatis/katbiznis
+
+/gulpfile.js - Gul tasks
+
+coded by Isatis
+
+started at 09/02/2017
+
+*/
+
+var gulp = require("gulp"),
+    babel = require("gulp-babel");
+
+
+
+// --- Task for js
+gulp.task("js", function() {
+  gulp.src("src/js/**/*.js")
+      .pipe(babel())
+      .pipe(gulp.dest("scripts"));
+});
+
+// --- Watch Tasks
+gulp.task("watch", function() {
+  gulp.watch("src/js/**/*.js", ["js"]);
+});
+
+// --- Aliases
+gulp.task("default", ["js"]);
+gulp.task("work", ["default", "watch"]);
